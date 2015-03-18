fbe2757d0623582f7260f47f0fbc766d87c88a05
Erick Barbosa <Erick@Ericks-iMac.local>
2015-03-18 14:28:02 -0400

    Initial Commit

---
+++ BmiCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BmiCalculator
+//
+//  Created by Erick Barbosa on 3/18/15.
+//  Copyright (c) 2015 Erick_Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BmiCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BmiCalculator
+//
+//  Created by Erick Barbosa on 3/18/15.
+//  Copyright (c) 2015 Erick_Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
